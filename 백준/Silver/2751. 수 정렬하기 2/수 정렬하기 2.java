1c2d43121962ce0a4a94cfb34cf9985b5e999849
Hyejin Jang <74856502+aaxx98@users.noreply.github.com>
2025-10-30 22:49:39 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1024 ms, Memory: 203680 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,31 +1,62 @@
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 public class Main {
 
   public static void main(String[] args) throws IOException {
-    List<Integer> list = new ArrayList<>();
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n];
     for (int i = 0; i < n; i++) {
-      int a = Integer.parseInt(br.readLine());
-      list.add(a);
+      arr[i] = Integer.parseInt(br.readLine());
     }
-    Collections.sort(list);
-    for (int i : list) {
+
+    mergeSort(arr, 0, n - 1);
+
+    for (int i : arr) {
       bw.write(i + "\n");
     }
- 
     bw.flush();
     bw.close();
     br.close();
+  }
+
+  public static void mergeSort(int[] arr, int left, int right) {
+    if (left >= right) {
+      return;
+    }
+    int mid = (left + right) / 2;
+
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+
+    merge(arr, left, mid, right);
+  }
+
+  public static void merge(int[] arr, int left, int mid, int right) {
+    int[] tmp = new int[right - left + 1];
+    int i = left, j = mid + 1, k = 0;
+
+    while (i <= mid && j <= right) {
+      if (arr[i] >= arr[j]) {
+        tmp[k++] = arr[j++];
+      } else {
+        tmp[k++] = arr[i++];
+      }
+    }
+
+    while (i <= mid) {
+      tmp[k++] = arr[i++];
+    }
+    while (j <= right) {
+      tmp[k++] = arr[j++];
+    }
+
+    System.arraycopy(tmp, 0, arr, left, tmp.length);
   }
 }
