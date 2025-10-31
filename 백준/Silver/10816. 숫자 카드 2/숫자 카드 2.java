1491b1341ecaeea28fc089bad2ac7c720ba45137
Hyejin Jang <74856502+aaxx98@users.noreply.github.com>
2025-10-31 21:06:55 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1660 ms, Memory: 181380 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,37 +1,57 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.StringTokenizer;
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-		int n, m, num;
-		n = Integer.parseInt(br.readLine());
-		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>(n+1);
-		st = new StringTokenizer(br.readLine());
-		for(int i=0;i<n;i++) {
-			num=Integer.parseInt(st.nextToken());
-			if(map.containsKey(num)) {
-				map.put(num, map.get(num)+1);
-			}
-			else {
-				map.put(num, 1);
-			}
-		}
-		m = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		for(int i=0;i<m;i++) {
-			num=Integer.parseInt(st.nextToken());
-			if(map.containsKey(num)) {
-				bw.write(map.get(num)+" ");
-			}
-			else {
-				bw.write("0 ");
-			}
-		}
-	
-		bw.flush();
-		bw.close();
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    int n = Integer.parseInt(br.readLine());
+    HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
+      hashMap.put(arr[i], hashMap.get(arr[i]) == null ? 1 : hashMap.get(arr[i]) + 1);
     }
+    Arrays.sort(arr);
+
+    int m = Integer.parseInt(br.readLine());
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < m; i++) {
+      int target = Integer.parseInt(st.nextToken());
+      int idx = binarySearch(target, arr);
+      if (idx == -1) {
+        bw.write("0 ");
+      } else {
+        bw.write(hashMap.get(target) + " ");
+      }
+    }
+
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+
+  public static int binarySearch(int target, int[] arr) {
+    int left = 0, right = arr.length - 1;
+    while (left <= right) {
+      int mid = (left + right) / 2;
+      if (arr[mid] == target) {
+        return mid;
+      } else if (arr[mid] < target) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+    return -1;
+  }
 }
